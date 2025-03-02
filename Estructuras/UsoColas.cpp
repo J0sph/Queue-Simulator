5287d39450c3f11a5fc32be3c9d880fc84e7d8d1
Alejandro Garcia <alejandro.garcia.rucavado@gmail.com>
2025-03-01 19:44:55 -0600

    Cambio a upgradeVIP

--- Estructuras/UsoColas.cpp
+++ Estructuras/UsoColas.cpp
@@ -147,13 +147,12 @@ void Elementos(sf::RenderWindow &window, Queue &queue, float y, sf::Color color,
 }
 
 // Metodo que permite que un grupo regular haga 'upgrade' a vip
-void upgradeToVip(int groupID){
+void upgradeToVip(int groupID, int prioridad){
     // Borra el grupo de la cola regular
     vector<string> integrantes = CRegular.deleteGroup(groupID);
    
     // Ingresa el grupo en la VIP
-    int longitudCola = CVIP.getLength();
-    CVIP.enqueue(integrantes, longitudCola);
+    CVIP.enqueue(integrantes, prioridad);
     
 }
 
